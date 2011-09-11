233ca37fe2b0d603299d195068c6ca3dc0a9f64c
Charlie Choiniere <nek4life@gmail.com>
2011-09-10 23:56:35 -0400

    Added bind statement to fix unboud mapper error

--- bunsen/paster_templates/pyramid_template/+package+/models/__init__.py
+++ bunsen/paster_templates/pyramid_template/+package+/models/__init__.py
@@ -10,5 +10,6 @@ Session = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 def initialize_sql(engine):
+    Base.metadata.bind = engine
     Base.metadata.create_all(engine)
     transaction.commit()
